8ee25c503a5e5406aa702ad02a15a35829c27991
Gülay Yolcu <28225625+gulayyolcu@users.noreply.github.com>
2021-11-04 06:08:26 +0300

    Add files via upload

---
+++ routes/movies.js
@@ -0,0 +1,95 @@
+const router=require("express").Router()
+const Movie=require("../models/Movie")
+const verify=require("../verifyToken")
+
+router.post("/",verify,async(req,res)=>{
+    if(req.user.id===req.params.id || req.user.isAdmin){
+        const movie=new Movie(req.body)
+
+        try{
+            const savedMovie=await movie.save()
+            res.status(200).json(savedMovie)
+        }catch(err){
+            res.status(200).json(err)
+        }
+    }else{
+        res.status(403).json("You are not allowed to!")
+    }
+})
+
+router.get("/find/:id",verify,async(req,res)=>{
+    if(req.user.id===req.params.id || req.user.isAdmin){
+        try{
+            const movie=await Movie.findById(req.params.id)
+            res.status(200).json(movie)
+        }catch(err){
+            res.status(200).json(err)
+        }
+    }else{
+        res.status(403).json("You are not allowed to!")
+    }
+})
+
+router.get("/",verify,async (req,res)=>{
+    if(req.user.isAdmin){
+    try{
+        const movies=await Movie.find()
+        res.status(200).json(movies.reverse())
+    }catch(err){
+        res.json(500).json(err)
+    }
+    }else{
+        res.status(403).json("You are not allowwed to!")
+    }
+   
+})
+
+router.put("/:id",verify,async (req,res)=>{
+    if(req.user.id===req.params.id || req.user.isAdmin){
+        try{
+            const updatedMovie=await Movie.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
+            res.status(200).json(updatedMovie)
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        res.status(403).json("You are not allowed to!")
+    }
+})
+
+router.delete("/:id",verify,async (req,res)=>{
+    if(req.user.id===req.params.id || req.user.isAdmin){
+        try{
+            await Movie.findByIdAndDelete(req.params.id)
+            res.status(200).json("Movie was deleted!")
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        res.status(403).json("You are not allowed to!")
+    }
+})
+
+router.get("/random",verify,async (req,res)=>{
+    const type=req.query.type
+    let movie;
+    try{
+        if(type==="series"){
+            movie=await Movie.aggregate([
+                {$match:{isSeries:true}},
+                {$sample:{size:1}}
+            ])
+        }else{
+            movie=await Movie.aggregate([
+                {$match:{isSeries:true}},
+                {$sample:{size:1}}
+            ])
+        }
+        res.status(200).json(movie)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+module.exports=router
